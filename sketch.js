f757f88848d849ff1c25d593d1cb6acab4a6f02c
rosalbadif <difonzorosalba@gmail.com>
2022-01-23 18:42:44 +0100

    didn't upload the code :/

--- sketch.js
+++ sketch.js
@@ -1,13 +1,42 @@
-function preload() {
-  // put preload code here
-}
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
 }
 
+
 function draw() {
-  // put drawing code here
-  background("red");
+
+  background("#121254")
+  rectMode (CENTER)
+  //create a grid
+  for(let x = 50; x < width; x += 50) {
+    for (let y = 50; y < height; y += 50) {
+    
+//let color change when mouse is on a rect
+      let distance = dist (mouseX, mouseY, x, y)
+     if (distance < 25) {
+       fill ("gold")
+       stroke("white")
+       strokeWeight (2)
+       rectMode (CENTER)
+       rect (x, y, 30, 40)
+       noFill()
+       rect (x-2.5, y-2.5, 25, 35)
+     }
+
+     else {
+       fill ("#c0c0f2")
+       stroke("black")
+       rectMode (CENTER)
+       rect (x, y, 30, 40)
+       noFill()
+       rect (x-2.5, y-2.5, 25, 35)
+     }
+    }
+ 
+
+  }
+  
 }
+
